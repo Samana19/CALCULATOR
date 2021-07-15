6acea7a07708757da605cf5b821a058ec70adde1
Samana Shrestha <samanashrestha02@gmail.com>
2021-07-15 14:00:29 +0545

    image

--- Calc.py
+++ Calc.py
@@ -30,14 +30,14 @@ root.iconbitmap('Calculator.ico')
 scvalue=StringVar()
 scvalue.set("")
 
-screen=Entry(root,width=50,fg="black", bg="white",borderwidth=10, text=scvalue, font=" Helvetica 40 bold")
-screen.pack(padx=80, pady=10)
+screen=Entry(root,width=50,fg="black", bg="white",borderwidth=20, text=scvalue, font=" Helvetica 40 bold")
+screen.pack(padx=70, pady=10)
 
 #Frames and buttons
 
-frame=Frame(root,bg="black")
+frame=Frame(root,highlightbackground="grey", highlightthickness=5,bg="black")
 
-button=Button(frame,text='C',bg='gray67',fg='black' ,font="Helvetica 32",padx=12,pady=5)
+button=Button(frame,text='C',bg='gray67',fg='black' ,font="Helvetica 32",padx=13,pady=5)
 button.pack(side=LEFT,padx=9,pady=10)
 button.bind("<Button-1>",click)
 
@@ -50,13 +50,13 @@ button=Button(frame,text='%',bg='gray67',fg='black' ,font="Helvetica 32",padx=12
 button.pack(side=LEFT,padx=9,pady=10)
 button.bind("<Button-1>",click)
 
-button=Button(frame,text='/',bg='DarkGoldenrod1',fg='white' ,font="Helvetica 32",padx=18,pady=5)
+button=Button(frame,text='/',bg='DarkGoldenrod1',fg='white' ,font="Helvetica 32",padx=22,pady=5)
 button.pack(side=LEFT,padx=9,pady=10)
 button.bind("<Button-1>",click)
 
 frame.pack()
 
-frame=Frame(root,bg="black")
+frame=Frame(root,highlightbackground="grey", highlightthickness=5,bg="black")
 
 button=Button(frame,text='7',bg='gray25',fg='white' ,font="Helvetica 35",padx=12,pady=5)
 button.pack(side=LEFT,padx=12,pady=18)
@@ -70,16 +70,16 @@ button=Button(frame,text='9',bg='gray25',fg='white' ,font="Helvetica 35",padx=12
 button.pack(side=LEFT,padx=12,pady=18)
 button.bind("<Button-1>",click)
 
-button=Button(frame,text='*',bg='DarkGoldenrod1',fg='white' ,font="Helvetica 35",padx=13,pady=5)
+button=Button(frame,text='*',bg='DarkGoldenrod1',fg='white' ,font="Helvetica 35",padx=15,pady=5)
 button.pack(side=LEFT,padx=12,pady=18)
 button.bind("<Button-1>",click)
 
 
 frame.pack()
 
-frame=Frame(root,bg="black")
+frame=Frame(root,highlightbackground="grey", highlightthickness=5,bg="black")
 
-button=Button(frame,text='4',bg='gray25',fg='white' ,font="Helvetica 35",padx=12,pady=5)
+button=Button(frame,text='4',bg='gray25',fg='white' ,font="Helvetica 35",padx=13,pady=5)
 button.pack(side=LEFT,padx=12,pady=18)
 button.bind("<Button-1>",click)
 
@@ -91,13 +91,13 @@ button=Button(frame,text='6',bg='gray25',fg='white' ,font="Helvetica 35",padx=12
 button.pack(side=LEFT,padx=12,pady=18)
 button.bind("<Button-1>",click)
 
-button=Button(frame,text='-',bg='DarkGoldenrod1',fg='white' ,font="Helvetica 35",padx=15,pady=5)
-button.pack(side=LEFT,padx=16,pady=18)
+button=Button(frame,text='-',bg='DarkGoldenrod1',fg='white' ,font="Helvetica 35",padx=13.25,pady=5)
+button.pack(side=LEFT,padx=14,pady=18)
 button.bind("<Button-1>",click)
 
 frame.pack()
 
-frame=Frame(root,bg="black")
+frame=Frame(root,highlightbackground="grey", highlightthickness=5,bg="black")
 
 button=Button(frame,text='1',bg='gray25',fg='white' ,font="Helvetica 35",padx=12,pady=5)
 button.pack(side=LEFT,padx=12,pady=18)
@@ -116,7 +116,7 @@ button.pack(side=LEFT,padx=11,pady=16)
 button.bind("<Button-1>",click)
 
 frame.pack()
-frame=Frame(root,bg="black")
+frame=Frame(root,highlightbackground="grey", highlightthickness=5,bg="black")
 
 button=Button(frame,text='0',bg='gray25',fg='white' ,font="Helvetica 35",padx=66,pady=5)
 button.pack(side=LEFT,padx=12,pady=18)
